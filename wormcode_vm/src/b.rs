489a9cb63751ff215cc25f88f5a789e3e559be8c
Nate Wilcox <nejucomo+dev@gmail.com>
2021-07-08 11:16:36 -0700

    Add handy Debug impls for B; failing unit test.

--- wormcode_vm/src/b.rs
+++ wormcode_vm/src/b.rs
@@ -1,8 +1,9 @@
+use std::fmt;
+
 /// A collection of N contiguous bits. N <= 32.
-#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+#[derive(Copy, Clone, PartialEq, Eq)]
 pub struct B<const N: usize>(u32);
 
-#[derive(Debug)]
 pub struct Overflow {
     bitsize: usize,
     input: u32,
@@ -41,5 +42,43 @@ impl<const N: usize> B<N> {
 
     pub fn concat<const J: usize, const K: usize>(a: B<J>, b: B<K>) -> B<N> {
         Self::from_u32((a.0 << K) & b.0)
+    }
+}
+
+impl<const N: usize> fmt::Debug for B<N> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut bits = vec![];
+        let mut n = self.0;
+        for i in 0..N {
+            bits.push(if n % 2 == 0 { "0" } else { "1" });
+            n >>= 1;
+            if (i + 1) % 4 == 0 {
+                bits.push(" ");
+                if (i + 1) % 8 == 0 {
+                    bits.push(" ");
+                }
+            }
+        }
+
+        bits.reverse();
+
+        write!(f, "B<{}>({})", N, bits.concat())
+    }
+}
+
+impl fmt::Debug for Overflow {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        struct Hexify(u32);
+
+        impl fmt::Debug for Hexify {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                write!(f, "0x{:x}", self.0)
+            }
+        }
+
+        f.debug_struct("Overflow")
+            .field("bitsize", &self.bitsize)
+            .field("input", &Hexify(self.input))
+            .finish()
     }
 }
