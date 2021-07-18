2ab174e2dde63fb2361498b6412c0cb3b9f5b78b
Nate Wilcox <nejucomo+dev@gmail.com>
2021-07-18 11:00:20 -0700

    Add encoding tests for `Mode`.

--- wormcode_vm/src/operand/mode.rs
+++ wormcode_vm/src/operand/mode.rs
@@ -12,3 +12,18 @@ impl From<Mode> for B<2> {
         B::<2>::from(m as u32)
     }
 }
+
+#[test]
+fn b2_from_literal() {
+    assert_eq!(B::<2>::from(0), B::<2>::from(Mode::Literal));
+}
+
+#[test]
+fn b2_from_direct() {
+    assert_eq!(B::<2>::from(1), B::<2>::from(Mode::Direct));
+}
+
+#[test]
+fn b2_from_indirect() {
+    assert_eq!(B::<2>::from(2), B::<2>::from(Mode::Indirect));
+}
