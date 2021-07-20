7b0f5846ac53197829a05fa6694623c2d361e79e
Nate Wilcox <nejucomo+dev@gmail.com>
2021-07-20 12:29:49 -0700

    Introduce internal `InstructionParser`.

--- wormcode_asm/src/parseinst.rs
+++ wormcode_asm/src/parseinst.rs
@@ -10,68 +10,76 @@ use wormcode_inst::{Instruction, Operand};
 
 pub fn parse_instruction(line: &str) -> ParseResult<Instruction> {
     let line = trim_comment(line);
-    let mut stream = TokenStream::from_line(line);
-    let mnemonic = stream.require_token("mnemonic")?;
-    let inst = stream_parse_instruction(&mut stream, mnemonic)?;
-    stream.finish()?;
-    Ok(inst)
+    InstructionParser::from_line(line).parse()
 }
 
-fn stream_parse_instruction<'a>(
-    stream: &mut TokenStream<'a>,
-    mnemonic: &str,
-) -> ParseResult<Instruction> {
-    use Instruction::*;
+struct InstructionParser<'a>(TokenStream<'a>);
 
-    match mnemonic {
-        "data" => parse_b(stream.require_token("datum")?).map(Data),
-        "nop" => Ok(Nop),
-        "step" => {
-            let a = parse_operand(stream)?;
-            Ok(Step(a))
-        }
-        "inc" => {
-            let a = parse_operand(stream)?;
-            let b = parse_operand(stream)?;
-            Ok(Inc(a, b))
-        }
-        "memcpy" => {
-            let a = parse_operand(stream)?;
-            let b = parse_operand(stream)?;
-            let c = parse_operand(stream)?;
-            Ok(MemCpy(a, b, c))
-        }
-        other => Err(ParseError::UnknownMnemonic(String::from(other))),
+impl<'a> InstructionParser<'a> {
+    fn from_line(line: &str) -> InstructionParser {
+        InstructionParser(TokenStream::from_line(line))
     }
-}
 
-fn parse_operand<'a>(stream: &mut TokenStream<'a>) -> ParseResult<Operand> {
-    use wormcode_inst::Mode::*;
+    fn parse(mut self) -> ParseResult<Instruction> {
+        let mnemonic = self.0.require_token("mnemonic")?;
+        let inst = self.parse_mnemonic(mnemonic)?;
+        self.0.finish()?;
+        Ok(inst)
+    }
 
-    fn unwrap_brackets(s: &str) -> ParseResult<Option<&str>> {
-        if let Some(t) = s.strip_prefix('[') {
-            if let Some(u) = t.strip_prefix(']') {
-                Ok(Some(u))
+    fn parse_mnemonic(&mut self, mnemonic: &str) -> ParseResult<Instruction> {
+        use Instruction::*;
+
+        match mnemonic {
+            "data" => parse_b(self.0.require_token("datum")?).map(Data),
+            "nop" => Ok(Nop),
+            "step" => {
+                let a = self.parse_operand()?;
+                Ok(Step(a))
+            }
+            "inc" => {
+                let a = self.parse_operand()?;
+                let b = self.parse_operand()?;
+                Ok(Inc(a, b))
+            }
+            "memcpy" => {
+                let a = self.parse_operand()?;
+                let b = self.parse_operand()?;
+                let c = self.parse_operand()?;
+                Ok(MemCpy(a, b, c))
+            }
+            other => Err(ParseError::UnknownMnemonic(String::from(other))),
+        }
+    }
+
+    fn parse_operand(&mut self) -> ParseResult<Operand> {
+        use wormcode_inst::Mode::*;
+
+        fn unwrap_brackets(s: &str) -> ParseResult<Option<&str>> {
+            if let Some(t) = s.strip_prefix('[') {
+                if let Some(u) = t.strip_prefix(']') {
+                    Ok(Some(u))
+                } else {
+                    Err(ParseError::Expected("close bracket ']'"))
+                }
             } else {
-                Err(ParseError::Expected("close bracket ']'"))
+                Ok(None)
+            }
+        }
+
+        let s = self.0.require_token("operand")?;
+        if let Some(t) = unwrap_brackets(s)? {
+            if let Some(u) = unwrap_brackets(t)? {
+                let b: B<6> = parse_b(u)?;
+                Ok(Operand::new(Indirect, b))
+            } else {
+                let b: B<6> = parse_b(t)?;
+                Ok(Operand::new(Direct, b))
             }
         } else {
-            Ok(None)
+            let b: B<6> = parse_b(s)?;
+            Ok(Operand::new(Literal, b))
         }
-    }
-
-    let s = stream.require_token("operand")?;
-    if let Some(t) = unwrap_brackets(s)? {
-        if let Some(u) = unwrap_brackets(t)? {
-            let b: B<6> = parse_b(u)?;
-            Ok(Operand::new(Indirect, b))
-        } else {
-            let b: B<6> = parse_b(t)?;
-            Ok(Operand::new(Direct, b))
-        }
-    } else {
-        let b: B<6> = parse_b(s)?;
-        Ok(Operand::new(Literal, b))
     }
 }
 
